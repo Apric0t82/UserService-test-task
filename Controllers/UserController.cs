c26ecf164783e4c56867cb734dd61e4517876b8e
Alex <apric0t@outlook.com>
2024-11-28 10:40:49 +0300

    UserController added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService_test_task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserDto userDto)
+        {
+            var userId = await _userService.CreateUserAsync(userDto);
+            return CreatedAtAction(nameof(FetchUsers), new { id = userId }, null);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FetchUsers()
+        {
+            var users = await _userService.GetUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpPut("role")]
+        public async Task<IActionResult> UpdateUserRole([FromBody] UpdateUserRoleDto roleDto)
+        {
+            await _userService.UpdateUserRoleAsync(roleDto);
+            return NoContent();
+        }
+    }
+}
